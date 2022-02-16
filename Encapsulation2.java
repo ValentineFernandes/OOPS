f3f8850b69284cedd0f56a0a74b11a442b02b7b1
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 10:27:20 +0530

    Update Encapsulation2.java

--- Encapsulation2.java
+++ Encapsulation2.java
@@ -1,13 +1,11 @@
-public class Encapsulation2 {
-   private String name;
-
-   // Getter
-   public String getName() {
-     return name;
-   }
-
-   // Setter
-   public void setName(String newName) {
-     this.name = newName;
-   }
+public class Encapsulation2 
+{
+  public static void main(String[] args) 
+  {
+    Encapsulation3 myObj = new Encapsulation3();
+    myObj.setName("John");
+    System.out.println(myObj.getName());
+  }
 }
+
+  
