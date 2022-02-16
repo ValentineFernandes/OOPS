3928677002c7e5aed3add328d296799b1b90df0c
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 10:35:21 +0530

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -1,15 +1,15 @@
 class Employee
 {
- float salary=40000;
+ float salary=50000;
 }
 
-class Programmer extends Employee
+class Boss extends Employee
 {
-  int bonus=10000;
+  int bonus=15000;
   public static void main(String args[])
   {
-   Programmer p=new Programmer();
-   System.out.println("Programmer salary is:"+p.salary);
-   System.out.println("Bonus of Programmer is:"+p.bonus);
+   Boss b=new Boss();
+   System.out.println("Boss salary is:"+b.salary);
+   System.out.println("Bonus of Boss is:"+b.bonus);
 }
 }
