546d5898a170ea3d4045d1191be03ef815682932
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 10:24:46 +0530

    Create Encapsulation.java

---
+++ Encapsulation.java
@@ -0,0 +1,9 @@
+public class Encapsulation 
+{
+  public static void main(String[] args) 
+  {
+    Encapsulation1 myObj = new Encapsulation1();
+    myObj.name = "John";
+    System.out.println(myObj.name);
+  }
+}
