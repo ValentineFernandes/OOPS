4a175568b8aa0f52a113d3f017961612b78512ac
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 10:26:28 +0530

    Create Encapsulation2.java

---
+++ Encapsulation2.java
@@ -0,0 +1,13 @@
+public class Encapsulation2 {
+   private String name;
+
+   // Getter
+   public String getName() {
+     return name;
+   }
+
+   // Setter
+   public void setName(String newName) {
+     this.name = newName;
+   }
+}
